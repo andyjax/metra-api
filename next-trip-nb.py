29d5cfe8ec267dfa4d95bf9f284a4693a27bbeaa
Jackson <andrew.jackson02@sap.com>
2019-02-22 09:38:34 -0600

    Updating next-trip script to support specifying a number of trips to display

--- next-trip-nb.py
+++ next-trip-nb.py
@@ -4,6 +4,19 @@ import requests
 import urllib3
 import datetime
 
+if len(sys.argv) == 2:
+    tripInput = sys.argv[1]
+    try:
+        numTrips = int(tripInput)
+    except ValueError:
+        print("Trip count must be an integer!")
+        sys.exit(2)
+    if (numTrips <= 0):
+        print("Trip count must be greater than 0!")
+        sys.exit(2)
+else:
+    numTrips = -1
+
 urllib3.disable_warnings()
 with open('credential.json') as creds:
     apiCred = json.load(creds)
@@ -24,6 +37,8 @@ response = requests.request("GET", tripUrl, data=payload, headers=headers)
 # identify UP-N trips
 
 for i in response.json():
+    if numTrips == 0:
+        break
     if (i['route_id']=="UP-N" and i['direction_id']==0 and i['service_id']=="A1"):
         # UP-N northbound, get trip stops to see if it stops at Central
         tripId = i['trip_id']
@@ -35,6 +50,7 @@ for i in response.json():
                 tripHour = int(tripTime[0])
                 if (tripHour >= currentHour):
                     print("Trip {0}, departs OTC at {1}".format(tripId, stops.json()[0]['departure_time']))
+                    numTrips -= 1
 
 # testUrl = "https://gtfsapi.metrarail.com/gtfs/schedule/stop_times/UP-N_UN314_V1_A"
 # testStop = requests.request("GET", testUrl, data=payload, headers=headers)
