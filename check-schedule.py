b1be3b6c7e1f7edcc9f688e1e3f4d1c289d6ad0f
Jackson <andrew.jackson02@sap.com>
2022-03-25 09:31:37 -0500

    Updating for UP-NW

--- check-schedule.py
+++ check-schedule.py
@@ -32,24 +32,41 @@ def ProgramMain():
         'cache-control': "no-cache"
     }
 
+    url = "https://gtfsapi.metrarail.com/gtfs/tripUpdates"
+    tripUpdates_response = requests.request("GET", url, data=payload, headers=headers)
+
     print("Morning Trip ID's to look for:")
     for j in tripsJson['morning']:
 #        print("{0}".format(j['trip_id']))
-        url = "https://gtfsapi.metrarail.com/gtfs/schedule/stop_times/" + j['trip_id']
-        response = requests.request("GET", url, data=payload, headers=headers)
-        for i in response.json():
-            if (i['stop_id']=="CENTRALST"):
-                print("{0}: Scheduled Central Street at {1}".format(j['trip_id'], i['arrival_time']))
+        api_schedule = [j['trip_id']+"_A", j['trip_id']+"_AA", j['trip_id']+"_B", j['trip_id']+"_C", j['trip_id']+"_D"]
+        printed = 0
+        otc_printed = 0
+        exists_printed = 0
+        for k in api_schedule:
+            url = "https://gtfsapi.metrarail.com/gtfs/schedule/stop_times/" + k
+            response = requests.request("GET", url, data=payload, headers=headers)
+#        url = "https://gtfsapi.metrarail.com/gtfs/schedule/stop_times/" + j['trip_id']
+#        response = requests.request("GET", url, data=payload, headers=headers)
+            for i in response.json():
+                if (i['stop_id']=="MTPROSPECT" and printed==0):
+                    print("{0}: \n\tScheduled Mount Prospect at {1}".format(j['trip_id'], i['arrival_time']))
+                    printed = 1
+                    if (i['center_boarding']==1):
+                        print('\033[93m' + "\tBoards from center platform!".format(j['trip_id']) + '\033[0m')
+                if (i['stop_id']=="OTC" and otc_printed==0):
+                    print("\tScheduled OTC at {0}".format(i['arrival_time']))
+                    otc_printed = 1
+            for i in tripUpdates_response.json():
+                if (k==i['id'] and exists_printed==0):
+                    delay = float(i['trip_update']['stop_time_update'][0]['arrival']['delay'])/60
+                    print('\033[1m' + "\t{0} exists! Delay is: {1} minutes".format(j['trip_id'], delay) + '\033[0m')
+                    exists_printed = 1
 
-    url = "https://gtfsapi.metrarail.com/gtfs/tripUpdates"
-
-    response = requests.request("GET", url, data=payload, headers=headers)
-
-    for morningTrip in tripsJson['morning']:
-        for i in response.json():
-            if (morningTrip['trip_id']==i['id']):
-                delay = float(i['trip_update']['stop_time_update'][0]['arrival']['delay'])/60
-                print("{0} exists! Delay is: {1} minutes".format(morningTrip['trip_id'], delay))
+#    for morningTrip in tripsJson['morning']:
+#        for i in response.json():
+#            if (morningTrip['trip_id']==i['id']):
+#                delay = float(i['trip_update']['stop_time_update'][0]['arrival']['delay'])/60
+#                print("{0} exists! Delay is: {1} minutes".format(morningTrip['trip_id'], delay))
 
 #    for i in response.json():
 #        if (i['id']=="UP-NW_UNW619_V1_B"):
